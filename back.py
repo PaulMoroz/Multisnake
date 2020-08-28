faf98cf14c389bf75f39b70f74fd53bf9585bce3
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-08-28 20:55:42 +0300

    Added base network thing

--- back.py
+++ back.py
@@ -1,5 +1,6 @@
 import socket
 import sqlite3
+import json
 import os
 from game import game,player
 
@@ -12,18 +13,25 @@ if not os.path.isfile("users.db"):
 conn = sqlite3.connect("users.db")
 cursor = conn.cursor()
 
-conn.execute('''CREATE TABLE users
+conn.execute('''CREATE TABLE IF NOT EXISTS users
              (id int,login text, pass text, win int, lose int, longest int)''')
 
 conn.commit()
 
 sock = socket.socket()
-sock.bind(('',9090))
+sock.bind(('',9091))
+
+print("DATABASE INITED")
 
 while True:
     sock.listen(1)
     connection, address = sock.accept()
-    buf = connection.recv(64)
+    buf = connection.recv(1024)
     if len(buf) > 0:
-        print(buf)
-    
+        resp = json.loads(str(buf).split("\\r\\n")[-1])
+        print(resp)
+
+connection.close()
+
+conn.close()
+
