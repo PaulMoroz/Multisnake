f93f5fd32895ee0e52abf2500b5eb9b13a4806c4
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-09-09 22:53:29 +0300

    Fixed few bugs and added base for menu

--- back.py
+++ back.py
@@ -2,9 +2,10 @@ import socket
 import sqlite3
 import json
 import os
-from game import game,player
+from game import game, player
 import random
 import string
+
 
 def get_random_string(length):
     letters = string.ascii_lowercase
@@ -19,12 +20,14 @@ cursor = conn.cursor()
 
 conn.execute('''CREATE TABLE IF NOT EXISTS users
              (id int,login varchar(256), pass varchar(256), win int, lose int, longest int,temp int)''')
-
+conn.commit()
+conn.execute('''CREATE TABLE IF NOT EXISTS games
+             (id int,name varchar(256), pass varchar(256),plOneId int,plTwoId int,plOneSnake text,plTwoSnake text,walls text,food text)''')
 conn.commit()
 
 sock = socket.socket()
 sock.bind(('',8080))
-
+games = {}
 print("DATABASE INITED")
 
 
@@ -51,31 +54,71 @@ def checkIfExists(player):
 
 def exit(player):
     cursor.execute("delete from users where login = '"+player['login']+"' and pass = '"+player['pass']+"' and id = '"+player['id']+"'")
-    return json.dumps({"opStatus":True})
+    return json.dumps({"opStatus": True})
 
-def router(cmd:dict):
+def getStat(player):
+    cursor.execute("select win,lose,longest from users where login = '"+player['login']+"' and id = '"+player['id']+"' and temp = '"+str(0 if player["mode"]!=3 else 1)+"'")
+    res = cursor.fetchone()
+    ans = {}
+    if res==None:
+        ans["opStatus"] = False
+    else:
+        ans["win"] = res[0]
+        ans["lose"] = res[1]
+        ans["longest"] = res[2]
+    return json.dumps(ans)
+   
+def getAllGames():      
+    cursor.execute("select win,lose,longest from users where login = '"+player['login']+"' and id = '"+player['id']+"' and temp = '"+str(0 if player["mode"]!=3 else 1)+"'")
+    res = cursor.fetchall()
+    ans = {}
+    ans["opStatus"] = True
+    ans["games"] = res
+    return json.dumps(ans)
+
+def createGame(game):
+    cursor.execute("select COUNT(id) from games where name = '"+game['name']+"'")
+    nums = cursor.fetchone()[0]
+    ans = {}
+    if nums!=0:
+        ans["opStatus"] = False
+    else:
+        ans["opStatus"] = True
+        cursor.execute("select COUNT(id) from users")
+        gid = cursor.fetchone()[0]+1
+        nume = [gid,game['name'],game['pass'],game['pid'],-1,str(""),"",""]
+        games[nume] = game.game()
+
+
+
+def router(cmd: dict):
     if cmd['cmd']=="register":
         return register(cmd["player"])
     elif cmd['cmd']=="exit":
         return exit(cmd["player"])
     elif cmd['cmd']=="check":
         return checkIfExists(cmd["player"])
+    elif cmd['cmd']=="getAllInfo":
+        return getAllInfo(cmd["player"])
+    elif cmd['cmd']=="getAllGames":
+        return getAllInfo()
+
 
 while True:
     sock.listen(1)
     connection, address = sock.accept()
     buf = connection.recv(1024)
     try:
-    if len(buf) > 0:
-        print(str(buf).split("\\r\\n"))
-        print(str(buf).split("\\r\\n")[-1][:-1])
-        resp = json.loads(str(buf).split("\\r\\n")[-1][:-1])
-        connection.send(("HTTP/1.1 200 OK\n"
-         +"Access-Control-Allow-Origin: *\n"
-         +"Content-Type: json\n"
-         +"\n"+router(resp)).encode('utf-8'))
+        if len(buf) > 0:
+            print(str(buf).split("\\r\\n"))
+            print(str(buf).split("\\r\\n")[-1][:-1])
+            resp = json.loads(str(buf).split("\\r\\n")[-1][:-1])
+            connection.send(("HTTP/1.1 200 OK\n"
+             + "Access-Control-Allow-Origin: *\n"
+             + "Content-Type: json\n"
+             + "\n"+router(resp)).encode('utf-8'))
     except:
-        print(buf)    
+        print(buf)
 
 
 connection.close()
